8bb9ea1ff0a550de080be5c4fe1b5def385111a4
tilikyan <tigranilikyan@gmail.com>
2022-05-05 15:20:14 +0300

    todo fix

--- src/main/java/com/example/demo/controller/EmployeeController.java
+++ src/main/java/com/example/demo/controller/EmployeeController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import com.example.demo.api.EmployeeInterface;
+import com.example.demo.exception.IdNotFoundException;
 import com.example.demo.model.Employee;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -12,7 +13,8 @@ import java.util.List;
 @RequiredArgsConstructor()
 public class EmployeeController {
 
-    private static final String successMessage = "success"; //todo у констат другой стиль именования
+    private static final String SUCCESS_MESSAGE = "success"; //todo у констат другой стиль именования
+                                                             // done
     private final EmployeeInterface employeeInterface;
 
     @GetMapping("/findAll")
@@ -23,18 +25,18 @@ public class EmployeeController {
     @PostMapping("/create")
     public String createEmployee(@RequestBody Employee employee) {
         employeeInterface.save(employee);
-        return successMessage;
+        return SUCCESS_MESSAGE;
     }
 
     @DeleteMapping("/delete/{id}")
-    public String deleteEmployee(@PathVariable String id) {
+    public String deleteEmployee(@PathVariable String id) throws IdNotFoundException {
         employeeInterface.deleteById(id);
-        return successMessage;
+        return SUCCESS_MESSAGE;
     }
 
     @PutMapping("/update")
-    public String updateEmployeesSalary(@RequestBody Employee employee) {
+    public String updateEmployeesSalary(@RequestBody Employee employee) throws Exception {
         employeeInterface.updateSalary(employee.getId(), employee.getSalary());
-        return successMessage;
+        return SUCCESS_MESSAGE;
     }
 }
--- src/main/java/com/example/demo/service/EmployeeService.java
+++ src/main/java/com/example/demo/service/EmployeeService.java
@@ -1,6 +1,7 @@
 package com.example.demo.service;
 
 import com.example.demo.api.EmployeeInterface;
+import com.example.demo.exception.IdNotFoundException;
 import com.example.demo.model.Employee;
 import com.example.demo.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +15,8 @@ import static java.lang.Integer.parseInt;
 
 @Service
 public class EmployeeService implements EmployeeInterface { //todo какой то метод не имплементировал
+                                                            // done вроде. Был метод, который я закомментил и не использовал. Сейчас вроде юзаю 4 из 4, которые
+                                                            // были в EmployeeInterface
     private final EmployeeRepository employeeRepository;
     private final MessageService messageService;
 
@@ -25,10 +28,7 @@ public class EmployeeService implements EmployeeInterface { //todo какой т
     }
 
     //todo не оставляй закоменченный код)
-//    @Override
-//    public Employee findById(int id) {
-//        return employeeRepository.findById(id).orElseThrow(getNotFoundIdExeption(id));
-//    }
+    // done
 
 
     @Override
@@ -37,7 +37,7 @@ public class EmployeeService implements EmployeeInterface { //todo какой т
     }
 
     @Override
-    public void deleteById(String id) {
+    public void deleteById(String id) throws IdNotFoundException {
         employeeRepository.findById(parseInt(id))
                 .orElseThrow(getNotFoundIdException(parseInt(id)));
         employeeRepository.deleteById(parseInt(id));
@@ -50,15 +50,16 @@ public class EmployeeService implements EmployeeInterface { //todo какой т
 
 
     @Override
-    public Employee updateSalary(int id, int salary) {
+    public Employee updateSalary(int id, int salary) throws IdNotFoundException {
         Employee updatedEmployee = employeeRepository.findById(id)
                 .orElseThrow(getNotFoundIdException(id));
         updatedEmployee.setSalary(salary);
         return employeeRepository.save(updatedEmployee);
     }
 
-    private Supplier<IllegalArgumentException> getNotFoundIdException(int id) {
+    private Supplier<IdNotFoundException> getNotFoundIdException(int id) {
         //todo IllegalArgumentException - тоже Runtime. Создай свое исключение от Exception
-        return () -> new IllegalArgumentException(messageService.getMessage("id.not.found.exceprion", id));
+        // done
+        return () -> new IdNotFoundException(messageService.getMessage("id.not.found.exception", id));
     }
 }
